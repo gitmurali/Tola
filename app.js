627bfbc24804680a2ed28b53e2a9b8c1de4cefd3
Murali Nutalapati <info@muraliprashanth.me>
2017-09-28 01:13:45 +0530

    adds more tests, controller to create document and test_helper

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/tola', {
+    useMongoClient: true,
+  });
+}
+
+app.use(bodyParser.json());
 routes(app);
 module.exports = app;
