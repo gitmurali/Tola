174148132f30f1c96490f72b9c679f6f9cc12a42
Murali Nutalapati <info@muraliprashanth.me>
2017-09-28 12:57:35 +0530

    cretes the document controller and send response on error

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,12 +4,13 @@ module.exports = {
   greeting(req, res) {
     res.send({hi: 'there'});
   },
-  create(req, res) {
+  create(req, res, next) {
     const driverProps = req.body;
 
     Driver.create(driverProps)
       .then(driver => {
         res.send(driver);
       })
+      .catch(next)
   }
 }
